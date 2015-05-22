ecee905c6308df52d22071cf99eaec141408a640
nooogent <nooogent2@gmail.com>
2015-05-22 13:09:32 +0100

    Remove unused bundles

--- GeolocationTestHarness/App_Start/BundleConfig.cs
+++ GeolocationTestHarness/App_Start/BundleConfig.cs
@@ -32,30 +32,30 @@ namespace GeolocationTestHarness
                       "~/Content/bootstrap-responsive.css"));
 
             //Uses geolocation
-            const string geolocationUri = "http://news.bbc.co.uk/";
+            //const string geolocationUri = "http://news.bbc.co.uk/";
 
-            bundles.Add(new ScriptBundle("~/bundles/geolocated_jquery", geolocationUri).Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/geolocated_jquery", geolocationUri).Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/geolocated_jqueryval", geolocationUri).Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/geolocated_jqueryval", geolocationUri).Include(
+            //            "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/geolocated_modernizr", geolocationUri).Include(
-                        "~/Scripts/modernizr-*", "~/Scripts/quest*"));
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/geolocated_modernizr", geolocationUri).Include(
+            //            "~/Scripts/modernizr-*", "~/Scripts/quest*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/geolocated_bootstrap", geolocationUri).Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/geolocated_bootstrap", geolocationUri).Include(
+            //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/geolocated_css", geolocationUri).Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/quest.css",
-                      "~/Content/quest1.css",
-                      "~/Content/quest2.css",
-                      "~/Content/quest3.css",
-                      "~/Content/bootstrap-responsive.css"));
+            //bundles.Add(new StyleBundle("~/Content/geolocated_css", geolocationUri).Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css",
+            //          "~/Content/quest.css",
+            //          "~/Content/quest1.css",
+            //          "~/Content/quest2.css",
+            //          "~/Content/quest3.css",
+            //          "~/Content/bootstrap-responsive.css"));
         }
     }
 }
