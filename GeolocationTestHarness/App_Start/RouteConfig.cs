68be580bbbcb46a9e180ef0b8caa42214f43189a
SpencerJoe <jsmelser@smelserstuart.com>
2015-05-25 10:10:55 +0100

    Fixes route config

--- GeolocationTestHarness/App_Start/RouteConfig.cs
+++ GeolocationTestHarness/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace GeolocationTestHarness
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Ping",
+                url: "ping",
+                defaults: new { controller = "Home", action = "Ping" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
